a7803528b0ce30aafbcca2425362346f014f3b06
Weeken Gao <weeken@koodar.com>
2017-08-02 11:37:05 +0800

    添加点击透明背景关闭窗口

--- src/cropper.vue
+++ src/cropper.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="v-cropper-bg-mask" @click="close">
+  <div class="v-cropper-bg-mask" @click.self="close">
     <div class="v-cropper">
       <span class="v-cropper-close" @click="close"></span>
       <div class="v-cropper-top">
